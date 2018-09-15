2c0e1e547f1ff180b8faf8c984aaeec41b01287d
DomainFlag <Cchivriga@hotmail.com>
2018-09-15 19:34:16 +0200

    refactoring

--- jiggles_native/app/src/main/java/com/example/cchiv/jiggles/Activities/ForumActivity.java
+++ jiggles_native/app/src/main/java/com/example/cchiv/jiggles/Activities/ForumActivity.java
@@ -5,12 +5,18 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.cchiv.jiggles.Adapters.ThreadAdapter;
 import com.example.cchiv.jiggles.R;
 import com.example.cchiv.jiggles.Utilities.NetworkUtilities;
 
 public class ForumActivity extends AppCompatActivity {
+
+    private static final String TAG = "ForumActivity";
 
     private RecyclerView recyclerView;
     private ThreadAdapter threadAdapter;
@@ -34,5 +40,26 @@ public class ForumActivity extends AppCompatActivity {
             threadAdapter.swapContent(threads);
             threadAdapter.notifyDataSetChanged();
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.forum_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.menu.forum_menu : {
+                // Handle the forum menu event
+                Log.v(TAG, String.valueOf(R.menu.forum_menu));
+            }
+            default: return false;
+        }
     }
 }
--- jiggles_native/app/src/main/java/com/example/cchiv/jiggles/Adapters/ThreadAdapter.java
+++ jiggles_native/app/src/main/java/com/example/cchiv/jiggles/Adapters/ThreadAdapter.java
@@ -1,21 +1,32 @@
 package com.example.cchiv.jiggles.Adapters;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.ImageViewCompat;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.cchiv.jiggles.Constants;
 import com.example.cchiv.jiggles.Model.Thread;
 import com.example.cchiv.jiggles.R;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 public class ThreadAdapter extends RecyclerView.Adapter<ThreadAdapter.ThreadViewHolder> {
 
@@ -43,26 +54,75 @@ public class ThreadAdapter extends RecyclerView.Adapter<ThreadAdapter.ThreadView
     private String parseDate(String objectId) {
         Date date = new Date(Integer.parseInt(objectId.substring(0, 8), 16) * 1000);
 
-        return date.toString();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy:MM:dd hh:mm", Locale.US);
+        return simpleDateFormat.format(date);
+    }
+
+    private void viewContrastizer(View view, int color) {
+        if(view instanceof LinearLayout) {
+            LinearLayout linearLayout = (LinearLayout) view;
+
+            int len = linearLayout.getChildCount();
+            for(int it = 0; it < len; it++) {
+                View child = linearLayout.getChildAt(it);
+
+                if(child instanceof TextView) {
+                    ((TextView) child).setTextColor(color);
+                } else if(child instanceof LinearLayout) {
+                    viewContrastizer(child, color);
+                } else if(child instanceof ImageView) {
+                    ImageView imageView = (ImageView) child;
+                    ImageViewCompat.setImageTintList(imageView, ColorStateList.valueOf(color));
+                }
+            }
+        }
     }
 
     @Override
     public void onBindViewHolder(@NonNull ThreadViewHolder holder, int position) {
         Thread thread = threads.get(position);
 
+        Uri uri = buildUriResource(thread.getCaption());
         Picasso.get()
-                .load(thread.getCaption())
-                .into(holder.thumbnail);
+                .load(uri)
+                .into(holder.thumbnail, new Callback() {
+                    @Override
+                    public void onSuccess() {
+                        BitmapDrawable bitmapDrawable = (BitmapDrawable) holder.thumbnail.getDrawable();
+
+                        if(bitmapDrawable != null) {
+                            Palette palette = Palette.from(bitmapDrawable.getBitmap()).generate();
+                            int lightMutedColor = palette.getLightMutedColor(ContextCompat.getColor(context, R.color.primaryTextColor));
+                            int darkMutedColor = palette.getDarkMutedColor(ContextCompat.getColor(context, R.color.primaryTextColor));
+
+                            viewContrastizer(holder.utilities, darkMutedColor);
+                            holder.itemView.setBackgroundColor(lightMutedColor);
+                        }
+                    }
+
+                    @Override
+                    public void onError(Exception e) {
+
+                    }
+                });
 
         holder.author.setText(thread.getAuthor().getName());
         holder.date.setText(parseDate(thread.getId()));
-//        holder.like.setText(thread.getVotes());
+        holder.like.setText(String.valueOf(thread.getVotes()));
 
         holder.comment.setOnClickListener((view) -> {
             // Do the comment
 
             Log.v(TAG, thread.getId());
         });
+    }
+
+    public Uri buildUriResource(String path) {
+        return new Uri.Builder()
+                .scheme(Constants.SCHEME)
+                .authority(Constants.AUTHORITY)
+                .appendEncodedPath(path)
+                .build();
     }
 
     @Override
@@ -83,6 +143,7 @@ public class ThreadAdapter extends RecyclerView.Adapter<ThreadAdapter.ThreadView
         private TextView author;
         private TextView date;
         private TextView comment;
+        private LinearLayout utilities;
 
         public ThreadViewHolder(View itemView) {
             super(itemView);
@@ -92,6 +153,7 @@ public class ThreadAdapter extends RecyclerView.Adapter<ThreadAdapter.ThreadView
             author = itemView.findViewById(R.id.author);
             date = itemView.findViewById(R.id.date);
             like = itemView.findViewById(R.id.like);
+            utilities = itemView.findViewById(R.id.utilities);
         }
     }
 }
